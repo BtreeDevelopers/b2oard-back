7a68e0e7cb0d7f1d02fd37d4a3bacf0a27798874
thalysm <42477970+thalysm@users.noreply.github.com>
2023-01-11 16:05:15 -0300

    teste novo

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ class App {
 
   private initialiseMiddleware(): void {
     this.express.get("/", (r, res) => {
-      res.send("taok");
+      res.send("taok 2");
     });
     this.express.use(helmet());
     this.express.use(cors());
