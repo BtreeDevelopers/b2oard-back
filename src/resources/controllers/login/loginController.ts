ec2ca85018d3a159964d7b297deb3b4a8f18214c
Paulo Ricardo <1981928paulo@gmail.com>
2023-01-19 12:05:29 -0300

    login?

--- src/resources/controllers/login/loginController.ts
+++ src/resources/controllers/login/loginController.ts
@@ -3,6 +3,11 @@ import { Router, Request, Response } from 'express';
 import z, { string } from 'zod';
 import { bauth } from '@/utils/bauth/bauth';
 import generateToken from '@/utils/Auth/jwt.auth';
+import mongoose from 'mongoose';
+import cardModel from '@/resources/models/cardModel';
+import boardModel from '@/resources/models/boardModel';
+import favoritesModel from '@/resources/models/favoritesModel';
+import raiaModel from '@/resources/models/raiaModel';
 
 class LoginController implements Controller {
     public path = '/login';
@@ -14,23 +19,17 @@ class LoginController implements Controller {
 
     public async initialiseRoutes(): Promise<void> {
         this.router.post(`${this.path}`, this.login);
+        this.router.delete(`${this.path}/:userid`, this.delete);
     }
 
     private async login(req: Request, res: Response): Promise<any> {
         try {
             const loginUser = z.object({
-                email: string().email(),
-                senha: string(),
+                token: string().email(),
+                userId: string(),
             });
 
-            const { email, senha } = loginUser.parse(req.body);
-
-            const responseLogin = await bauth.post('/login', {
-                email: email,
-                senha: senha,
-            });
-
-            const { token, user } = await responseLogin.data;
+            const { token, userId } = loginUser.parse(req.body);
 
             bauth.defaults.headers.common = {
                 Authorization: `bearer ${token}`,
@@ -38,10 +37,10 @@ class LoginController implements Controller {
 
             const responseTokenUser = await bauth.get('/user');
 
-            const userFromToken = responseTokenUser.data;
+            const user = responseTokenUser.data;
 
-            if (user._id === userFromToken._id) {
-                const token_bjrd = generateToken({ id: userFromToken._id });
+            if (user._id === userId) {
+                const token_bjrd = generateToken({ id: userId });
                 return res.status(200).json({
                     token_bjrd,
                     _id: user._id,
@@ -58,6 +57,110 @@ class LoginController implements Controller {
         } catch (error: any) {
             console.log(error);
             return res.status(400).json({ message: 'Ocorreu um erro' });
+        }
+    }
+    private async delete(req: Request, res: Response): Promise<any> {
+        const session = await mongoose.startSession();
+        session.startTransaction();
+        try {
+            const userid = req.params.userid;
+
+            const board_proprietario = await boardModel.find({ owner: userid });
+            const ids_board_proprietario_deletar: string[] = [];
+            const ids_board_proprietario_transferencia: string[] = [];
+
+            board_proprietario.forEach((element) => {
+                // se há apenas um followers ele é o dono do board, logo não há quem promover a dono
+                if (element.followers.length === 1) {
+                    ids_board_proprietario_deletar.push(...element._id);
+                } else {
+                    ids_board_proprietario_transferencia.push(...element._id);
+                }
+            });
+
+            //apagando os boards em que é proprietário
+            const raias_proprietario_deletar = await raiaModel.find({
+                board: { $in: ids_board_proprietario_deletar },
+            });
+
+            const ids_cards_proprietario_deletar: string[] = [];
+
+            raias_proprietario_deletar.forEach((element) => {
+                element.cards.forEach((element_card) => {
+                    ids_cards_proprietario_deletar.push(element_card);
+                });
+            });
+
+            await cardModel.deleteMany({
+                _id: { $in: ids_cards_proprietario_deletar },
+            });
+            await raiaModel.deleteMany({
+                board: { $in: ids_board_proprietario_deletar },
+            });
+            await boardModel.deleteMany({
+                _id: { $in: ids_board_proprietario_deletar },
+            });
+
+            //removendo do favoritos dos boards que era dono e mudando a propriedade
+            const board_proprietario_transferencia = await boardModel.find({
+                _id: { $in: ids_board_proprietario_transferencia },
+            });
+
+            await boardModel.updateMany(
+                { _id: { $in: ids_board_proprietario_transferencia } },
+                {
+                    $pull: { followers: userid },
+                }
+            );
+
+            const newOwner: { _id: string; owner: string }[] = [];
+            board_proprietario_transferencia.forEach((element) => {
+                newOwner.push({
+                    _id: element._id,
+                    owner:
+                        element.followers[0] !== userid
+                            ? element.followers[0]
+                            : element.followers[1],
+                });
+            });
+
+            for (let i = 0; i < newOwner.length; i++) {
+                boardModel.updateOne(
+                    { _id: newOwner[i]._id },
+                    {
+                        $set: { owner: newOwner[i].owner },
+                    }
+                );
+            }
+
+            //apagando favoritos
+            await favoritesModel.findOneAndDelete({
+                userId: userid,
+            });
+
+            //removendo dos boards do qual não é dono o seguir
+            await boardModel.updateMany(
+                {},
+                {
+                    $pull: { followers: userid },
+                }
+            );
+
+            await cardModel.updateMany(
+                {},
+                {
+                    $pull: { followers: userid },
+                }
+            );
+
+            await session.commitTransaction();
+            return res.status(200).json({ board_proprietario });
+        } catch (error) {
+            console.log(error);
+            await session.abortTransaction();
+            return res.status(500).json(error);
+        } finally {
+            session.endSession();
         }
     }
 }
